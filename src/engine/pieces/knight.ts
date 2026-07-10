3ec4969147d47c20712c567ea8fa9278aaf5ece8
Alex Fechet <alex.fechet@softwire.com>
2026-07-10 13:56:14 +0300

    added the functionality of the knight

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,21 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentPos = board.findPiece(this);
+        let availableMoves : Square[] = [];
+
+        let di: number[] = [2, 2, -2, -2, 1, 1, -1, -1];``
+        let dj: number[] = [1, -1, 1, -1, 2, -2, 2, -2];
+
+        for(let k = 0; k < di.length; k++) {
+            let newRow = currentPos.row + di[k];
+            let newCol = currentPos.col + dj[k];
+
+            if(Square.checkDimensions(newRow, newCol)) {
+                availableMoves.push(Square.at(newRow, newCol));
+            }
+        }
+
+        return availableMoves;
     }
 }
