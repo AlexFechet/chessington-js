5381b9dbe58d8d5b0fd0f2ef81ae347dce132420
Alex Fechet <alex.fechet@softwire.com>
2026-07-10 13:15:32 +0300

    moves for the rock in each directions

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,19 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+
+        let availableMoves : Square[] = [];
+        let currentPos = board.findPiece(this);
+
+        for(let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            if(i != currentPos.row) {
+                availableMoves.push(Square.at(i, currentPos.col));
+            }
+            if(i != currentPos.col) {
+                availableMoves.push(Square.at(currentPos.row, i));
+            }
+        }
+
+        return availableMoves;
     }
 }
