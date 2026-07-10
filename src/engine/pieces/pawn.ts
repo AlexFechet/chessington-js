d3943c93a6f968dc86358eed31e2fb05a8b5c800
Alex Fechet <alex.fechet@softwire.com>
2026-07-10 12:23:37 +0300

    rezolved red-1, pawn can mode 1 square

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves : Square[] = [];
+        let currentPos = board.findPiece(this);
+
+        if(this.player == Player.WHITE) {
+            availableMoves.push(Square.at(currentPos.row + 1, currentPos.col));
+        } else {
+            availableMoves.push(Square.at(currentPos.row - 1, currentPos.col));
+        }
+
+        return availableMoves;
     }
 }
